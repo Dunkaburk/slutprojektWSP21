f3e0eb3fcb2a85487418ce6c7a7defe97b95d71c
Dunkaburk <Dunkaburk@users.noreply.github.com>
2021-03-26 11:14:27 +0100

    Fixat problem med register

--- app.rb
+++ app.rb
@@ -8,9 +8,11 @@ enable :sessions
 
 
 get ("/") do
-    db = SQLite3::Database.new("db/schema.db")
+    p "Register load"
+    db = connect_to_db
     db.results_as_hash = true
-    classes = db.execute("SELECT class_name FROM class")
+    classes = get_classes
+    p classes
     slim(:register, locals:{klasser:classes})
 end
 
@@ -21,14 +23,16 @@ end
 post('/users/new') do
     username = params[:username]
     password = params[:password]
+    class_id = params[:classroom]
     pw_confirm = params[:password_confirm]
+    p class_id
 
     #register_user(username, password, pw_confirm)
 
     if (pw_confirm == password)
         password_digest = BCrypt::Password.create(password)
         db = SQLite3::Database.new("db/schema.db")
-        db.execute("INSERT INTO students (username,pwdigest) VALUES (?,?)",username,password_digest)
+        db.execute("INSERT INTO students (username,pwdigest,class_id) VALUES (?,?, ?)",username,password_digest,class_id)
         redirect("/showlogin")
       
       else
@@ -62,11 +66,12 @@ post("/login") do
 end
 
 
+
 get("/schedule") do
     id = session[:id].to_i
     db = SQLite3::Database.new("db/schema.db")  
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM schedule WHERE user_id = ?",id)
+    result = db.execute("SELECT * FROM schedule WHERE student_id = ?",id)
     p "Visar ditt schema #{result}"
     slim(:"schedule/index", locals:{scheman:result})
 
